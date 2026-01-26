1e3d0c69c0176355e6dc48a31d846d261cc248ce
WIN-M1JFA527P1T\user <sabina060807@gmail.com>
2026-01-26 17:35:12 -0500

    commented the code

--- Assets/scripts/RotateMe.cs
+++ Assets/scripts/RotateMe.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class RotateMe : MonoBehaviour
 {
     public float speed = 1;
+    //so i can modify it in the inspector window
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +14,7 @@ public class RotateMe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //the rotation 
         Vector3 newRotation = transform.eulerAngles;
         newRotation.z += speed * Time.deltaTime;
         transform.eulerAngles = newRotation;
